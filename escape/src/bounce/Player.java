bf007ca4c1cb702dc9d8a270712353dbe0551f38
John Stevenson <j.stevenson8@students.clark.edu>
2021-10-05 12:23:01 -0700

    Initial version of user control

--- escape/src/bounce/Player.java
+++ escape/src/bounce/Player.java
@@ -1,13 +1,20 @@
 package bounce;
 
 import jig.ResourceManager;
+import jig.Vector;
 
 public class Player extends Character {
+    private Vector velocity;
 
     public Player(int x, int y) {
         super(x,y);
         addImageWithBoundingBox(ResourceManager.getImage(EscapeGame.PLAYER_IMG_RSC));
+        velocity = new Vector(0f, 0f);
     }
 
+    public void setVelocity(final Vector v) { this.velocity = v; }
 
+    public void update(int delta) {
+        translate(velocity.scale(delta));
+    }
 }
--- escape/src/bounce/StartUpState.java
+++ escape/src/bounce/StartUpState.java
@@ -1,7 +1,9 @@
 package bounce;
 
+import jig.Vector;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -31,6 +33,20 @@ public class StartUpState extends BasicGameState {
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
+        EscapeGame eg = (EscapeGame) stateBasedGame;
+        Input input = gameContainer.getInput();
+
+        if (input.isKeyDown(Input.KEY_DOWN)) {
+            eg.player.setVelocity(new Vector(0f, 1f));
+        } else if (input.isKeyDown(Input.KEY_UP)) {
+            eg.player.setVelocity(new Vector(0f, -1f));
+        } else if (input.isKeyDown(Input.KEY_LEFT)) {
+            eg.player.setVelocity(new Vector(-1f, 0f));
+        } else if (input.isKeyDown(Input.KEY_RIGHT)) {
+            eg.player.setVelocity(new Vector(1f, 0f));
+        }
+
+        eg.player.update(i);
 
     }
 }
