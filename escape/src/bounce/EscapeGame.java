927e310bf013d4904488bd835fef5c54ada83f59
John Stevenson <j.stevenson8@students.clark.edu>
2021-10-04 22:09:52 -0700

    Player and StartUpState created; Player rendered

---
+++ escape/src/bounce/EscapeGame.java
@@ -0,0 +1,64 @@
+package bounce;
+
+import java.util.ArrayList;
+
+import jig.Entity;
+import jig.ResourceManager;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class EscapeGame extends StateBasedGame {
+	public final int ScreenWidth;
+	public final int ScreenHeight;
+
+	public static final String PLAYER_IMG_RSC = "bounce/resource/player.png";
+
+	public static final int STARTUPSTATE = 0;
+
+	Player player;
+
+	/**
+	 * Create the BounceGame frame, saving the width and height for later use.
+	 * 
+	 * @param title
+	 *            the window's title
+	 * @param width
+	 *            the window's width
+	 * @param height
+	 *            the window's height
+	 */
+	public EscapeGame(String title, int width, int height) {
+		super(title);
+		ScreenHeight = height;
+		ScreenWidth = width;
+		Entity.setCoarseGrainedCollisionBoundary(Entity.AABB);
+	}
+
+
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException {
+		addState(new StartUpState());
+
+		ResourceManager.loadImage(PLAYER_IMG_RSC);
+
+		player = new Player(100,100);
+	}
+	
+	public static void main(String[] args) {
+		AppGameContainer app;
+		try {
+			app = new AppGameContainer(new EscapeGame("Escape!", 800, 600));
+			app.setDisplayMode(800, 600, false);
+			app.setVSync(true);
+			app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	
+}
